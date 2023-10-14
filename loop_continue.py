0224be4d63d316eb0aaf515e1dced789d15f14fb
Jany Muong <me.roumuong@gmail.com>
2023-10-14 08:54:39 +0300

    git rm --cached loop_break_chars.py loop_continue.py

--- loop_continue.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-for i in range(5):
-    '''
-    statement is used to skip the rest of the current iteration of a loop
-    and move to the next iteration.
-    '''
-    if i == 2:
-        continue
-    print(i)
